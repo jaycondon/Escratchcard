87e4bd515cf6fbe9311e8985fc3c51fcb551a5b2
John Condon <johnnycndn@yahoo.ie>
2015-04-14 18:02:11 +0100

    Fixed dependencies

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='ESCGProject',
     version='1.0',
     long_description=__doc__,
-    packages=['runserver'],
+    packages=['ESCGProject'],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
