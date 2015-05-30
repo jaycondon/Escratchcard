815a8abd50946b01846c069d1b2641c76b29866e
John Condon <johnnycndn@yahoo.ie>
2015-05-30 13:26:27 +0100

    uncommented setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
-# from setuptools import setup
+from setuptools import setup
 
-# setup(
-#     name='ESCGProject',
-#     version='1.0',
-#     long_description=__doc__,
-#     packages=['ESCGProject'],
-#     include_package_data=True,
-#     zip_safe=False,
-#     install_requires=[
-#     'Flask>=0.10',
-#     'PyMySQL>=0.6',
-#     'Flask-WTF>=0.11',
-#     'paypalrestsdk>=1.1.0',
-#     'pycrypto>=2.6.0',
-#     'flask-bootstrap>=3.0'
-# 	]
-# )
+setup(
+    name='ESCGProject',
+    version='1.0',
+    long_description=__doc__,
+    packages=['ESCGProject'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+    'Flask>=0.10',
+    'PyMySQL>=0.6',
+    'Flask-WTF>=0.11',
+    'paypalrestsdk>=1.1.0',
+    'pycrypto>=2.6.0',
+    'flask-bootstrap>=3.0'
+	]
+)
