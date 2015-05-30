57e23d447c183930aea48e2c05bae111d04f5512
John Condon <johnnycndn@yahoo.ie>
2015-05-30 11:57:28 +0100

    changed app.run to removed context

--- runserver.py
+++ runserver.py
@@ -8,9 +8,9 @@ from ESCGProject.database import init_db
 app.secret_key = 'some_secret'
 
 
-import ssl
-context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-context.load_cert_chain('ESCGProject/cacert.pem', 'ESCGProject/privkey.pem')
+#import ssl
+#context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+#context.load_cert_chain('ESCGProject/cacert.pem', 'ESCGProject/privkey.pem')
 
 init_db()
 
