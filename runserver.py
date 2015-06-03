1ae711b776b0d989dcfdd408e63cd2b52cebd6bb
John Condon <johnnycndn@yahoo.ie>
2015-06-03 12:30:26 +0100

    added ssl back in

--- runserver.py
+++ runserver.py
@@ -6,12 +6,12 @@ import paypalrestsdk
 from ESCGProject.database import init_db
 
 
-#import ssl
-#context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-#context.load_cert_chain('ESCGProject/cacert.pem', 'ESCGProject/privkey.pem')
+import ssl
+context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+context.load_cert_chain('ESCGProject/cacert.pem', 'ESCGProject/privkey.pem')
 
 init_db()
 
 #app.debug=True
 #app.run()
-app.run()#host='127.0.0.1', host='0.0.0.0',ssl_context=context
+app.run(ssl_context=context)#host='127.0.0.1', host='0.0.0.0',ssl_context=context
